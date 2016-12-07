026cc8e697e2550c982ef1828a98ca3a3e72678d
1036012350@qq.com <JTQc5235>
2016-12-07 16:25:35 +0800

    访问了一个网络的天气接口

--- retrofittest/src/main/java/winning/retrofittest/MainActivity.java
+++ retrofittest/src/main/java/winning/retrofittest/MainActivity.java
@@ -13,6 +13,7 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.GET;
 import retrofit2.http.Path;
+import retrofit2.http.Query;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,6 +21,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+//        GetMyFirstData();
+        GetMyWeatherData();
+    }
+
+    private void GetMyFirstData() {
         Retrofit retrofit = new Retrofit.Builder()
                 .baseUrl("http://gank.io/api/data/")
                 .addConverterFactory(GsonConverterFactory.create())
@@ -39,8 +45,33 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private void GetMyWeatherData() {
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("http://wthrcdn.etouch.cn/")
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+        IWeatherBiz userBiz = retrofit.create(IWeatherBiz.class);
+        Call<Object> call = userBiz.getWeatherReturn("芜湖");
+        call.enqueue(new Callback<Object>() {
+            @Override
+            public void onResponse(Call<Object> call, Response<Object> response) {
+                Log.e("infoooo", "normalGet:" + response.body() + "");
+            }
+
+            @Override
+            public void onFailure(Call<Object> call, Throwable t) {
+                Log.e("infoooo", "normalGet:" + t.toString() + "");
+            }
+        });
+    }
+
     public interface IUserBiz {
         @GET("all/20/{page}")
         Call<ReturnBean> getReturn(@Path("page") int page);
+    }
+
+    public interface IWeatherBiz {
+        @GET("weather_mini")
+        Call<Object> getWeatherReturn(@Query("city") String city);
     }
 }
